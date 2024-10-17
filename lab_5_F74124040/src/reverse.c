c1182a85eba37ef4bf8a977ed97e0ff36aca98ab
NoraYen555 <norayen555@gmail.com>
2024-10-17 17:16:42 +0800

    Debug reverse.c

--- lab_5_F74124040/src/reverse.c
+++ lab_5_F74124040/src/reverse.c
@@ -1,8 +1,8 @@
 #include "../include/reverse.h"
 char *reverse(char *dest, const char *src) 
 {
-	for(int i = 0; i < 10; i++){
-		dest[i] = src[10 - i - 1];
+	for(int i = 0; i < 9; i++){
+		dest[i] = src[9 - i - 1];
 	}
 	dest[10] = '\0';
 	return dest;
